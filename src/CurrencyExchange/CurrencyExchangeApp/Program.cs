7ad7a4c3d1a8d51a5c1861bbcc4503949af56fcf
Aslamov Yury <aslamov.yura@gmail.com>
2020-04-22 09:20:17 +0300

    init: add CurrencyExchange

---
+++ src/CurrencyExchange/CurrencyExchangeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyExchangeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

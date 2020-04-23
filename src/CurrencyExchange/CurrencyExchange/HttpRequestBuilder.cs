cab3cce4ca4f5ebff7d719850ee66f2eb0e8b704
Aslamov Yury <aslamov.yura@gmail.com>
2020-04-23 11:46:56 +0300

    tests: add HttpRequestBuilder tests

--- src/CurrencyExchange/CurrencyExchange/HttpRequestBuilder.cs
+++ src/CurrencyExchange/CurrencyExchange/HttpRequestBuilder.cs
@@ -15,22 +15,30 @@ namespace CurrencyExchange
         public HttpRequestBuilder() { }
 
         /// <summary>
+        /// Initialize httpRequestBuilder with root URL.
+        /// </summary>
+        public HttpRequestBuilder(string rootUrl)
+        {
+            _root = rootUrl ?? throw new ArgumentNullException();
+        }
+
+        /// <summary>
         /// Get request for the information about the whole currencies at nbrb.by.
         /// </summary>
         /// <returns></returns>
         public string GetCurrencyInfoRequest() => string.Concat(_root, "currencies/");
 
         /// <summary>
-        /// Get request for the information about the currency with a specific index (at nbrb.by).
+        /// Get request for the information about the currency with a specific Identifier (accordint to nbrb.by).
         /// </summary>
-        /// <param name="currencyIndex">Currency index at nbrb.by.</param>
-        /// <returns></returns>
-        public string GetCurrencyInfoRequest(int currencyIndex)
+        /// <param name="currencyId">Currency ID according to nbrb.by.</param>
+        /// <returns>URL request.</returns>
+        public string GetCurrencyInfoRequest(int currencyId)
         {
-            if (currencyIndex < 0)
+            if (currencyId < 0)
                 throw new ArgumentOutOfRangeException();
 
-            return string.Concat(_root, "currencies/", currencyIndex.ToString());
+            return string.Concat(_root, "currencies/", currencyId.ToString());
         }
 
         /// <summary>
@@ -40,16 +48,59 @@ namespace CurrencyExchange
         public string GetCurrencyRateRequest() => string.Concat(_root, "rates?periodicity=0");
 
         /// <summary>
-        /// Get request for the rate of the currency with a specific index (at nbrb.by).
+        /// Get request for the rate of the currency with a specific digital code (according to ISO-4217).
         /// </summary>
-        /// <param name="currencyIndex">Index of currency at nbrb.by.</param>
-        /// <returns></returns>
-        public string GetCurrencyRateRequest(int currencyIndex)
+        /// <param name="digitalCode">Digital code of currency according to ISO-4217.</param>
+        /// <returns>URL request.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public string GetCurrencyRateRequest(int digitalCode)
         {
-            if (currencyIndex < 0)
+            if (digitalCode < 0)
                 throw new ArgumentOutOfRangeException();
 
-            return string.Concat(_root, "rates/", currencyIndex.ToString(), "?periodicity=0");
+            return string.Concat(_root, "rates/", digitalCode.ToString(), "?parammode=1");
+        }
+
+        /// <summary>
+        /// Get request for the rate of the currency with a specific abbreviation according to ISO-4217 (USD, EUR, RUB).
+        /// </summary>
+        /// <param name="curAbbreviation">Currency ISO-4217 abbreviation (USD, EUR, RUB).</param>
+        /// <returns>URL request.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public string GetCurrencyRateRequest(string curAbbreviation)
+        {
+            curAbbreviation = curAbbreviation ?? throw new ArgumentNullException();
+
+            return string.Concat(_root, "rates/", curAbbreviation.ToUpper(), "?parammode=2");
+        }
+
+        /// <summary>
+        /// Get request for the rate of the currency with a specific abbreviation according to ISO-4217 (USD, EUR, RUB).
+        /// </summary>
+        /// <param name="currencyId">Currency ISO-4217 abbreviation (USD, EUR, RUB).</param>
+        /// <returns>URL request.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public string GetCurrencyRateRequest(int currencyId, DateTime date)
+        {
+            if (currencyId < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (date == default)
+                throw new ArgumentNullException();
+
+            string dateString;
+            try
+            {
+                dateString = date.ToString("yyyy-MM-dd");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid date-time format!");
+                return null;
+            }
+            
+            return string.Concat(_root, "rates/", currencyId, "?ondate=", dateString);
         }
     }
 }
--- tests/CurrencyExchangeTests/HttpRequestBuilderTests.cs
+++ tests/CurrencyExchangeTests/HttpRequestBuilderTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using CurrencyExchange;
+using System;
 
 namespace CurrencyExchangeTests
 {
@@ -13,8 +14,132 @@ namespace CurrencyExchangeTests
         }
 
         [Fact]
-        public void Test1()
+        public void RequestBuilder_WhenInitWithNullUrl_Return_ArgumentNullException()
         {
+            // Arrange
+            HttpRequestBuilder requestBuilder;
+            string rootUrl = null;
+            bool isException = false;
+
+            // Act
+            try
+            {
+                requestBuilder = new HttpRequestBuilder(rootUrl);
+            }
+            catch (ArgumentNullException)
+            {
+                isException = true;
+            }
+
+            // Assert
+            Assert.True(isException);
+        }
+
+        [Fact]
+        public void RequestBuilder_WhenGetCurrencyInfoRequestWithNegativeIf_Return_ArgumentOutOfRangeException()
+        {
+            // Arrange
+            int currencyId = -5;
+            bool isException = false;
+
+            // Act
+            try
+            {
+                requestBuilder.GetCurrencyInfoRequest(currencyId);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                isException = true;
+            }
+
+            // Assert
+            Assert.True(isException);
+        }
+
+        [Fact]
+        public void RequestBuilder_WhenGetCurrencyRateRequestWithNegativeCode_Return_ArgumentOutOfRangeException()
+        {
+            // Arrange
+            int currencyCode = -5;
+            bool isException = false;
+
+            // Act
+            try
+            {
+                requestBuilder.GetCurrencyRateRequest(currencyCode);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                isException = true;
+            }
+
+            // Assert
+            Assert.True(isException);
+        }
+
+        [Fact]
+        public void RequestBuilder_WhenGetCurrencyRateRequestWithNullCurrencyAbbreviation_Return_ArgumentNullException()
+        {
+            // Arrange
+            string curAbbreviation = null;
+            bool isException = false;
+
+            // Act
+            try
+            {
+                requestBuilder.GetCurrencyRateRequest(curAbbreviation);
+            }
+            catch (ArgumentNullException)
+            {
+                isException = true;
+            }
+
+            // Assert
+            Assert.True(isException);
+        }
+
+        [Fact]
+        public void RequestBuilder_WhenGetCurrencyRateRequestWithNegativeIdAndNormalDate_Return_ArgumentOutOfRangeException()
+        {
+            // Arrange
+            int currencyId = -5;
+            DateTime date = DateTime.Today;
+            bool isException = false;
+
+            // Act
+            try
+            {
+                requestBuilder.GetCurrencyRateRequest(currencyId, date);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                isException = true;
+            }
+
+            // Assert
+            Assert.True(isException);
+        }
+
+        [Fact]
+        public void RequestBuilder_WhenGetCurrencyRateRequestWithNormalIdAndDefaulDate_Return_ArgumentNullException()
+        {
+            // Arrange
+            int currencyId = 1;
+            DateTime date = default;
+            bool isException = false;
+
+            // Act
+            try
+            {
+                requestBuilder.GetCurrencyRateRequest(currencyId, date);
+            }
+            catch (ArgumentNullException)
+            {
+                isException = true;
+            }
+
+            // Assert
+            Assert.True(isException);
         }
     }
 }
